31475f4cf19f2cc6a30df6e1f992c626d34c2042
Noriki Nakamura <bynn_chapu@tencho-lab.net>
2022-08-09 23:32:42 +0900

    Use OS environment variable for TARGET HOST instead of input.

--- src/send-value-to-zabbix.py
+++ src/send-value-to-zabbix.py
@@ -1,7 +1,17 @@
 import json
+import os
 from pyzabbix import ZabbixMetric, ZabbixSender
 
 def send_metric_data(zabbix_data):
+    try:
+        zabbix_server_host = os.environ['ZABBIX_SERVER_HOST']
+    except KeyError:
+        print ('Error: Environment variable for ZABBIX_SERVER_HOST is missing.')
+        result_info = {
+            'failed': 1
+        }
+        return result_info
+
     print('[Sending data]')
     print(json.dumps(zabbix_data))
 
@@ -11,7 +21,7 @@ def send_metric_data(zabbix_data):
                                zabbix_data['target_value'])
     send_data.append(metric_data)
 
-    zabbix_server = ZabbixSender(zabbix_data['zabbix_server_host'])
+    zabbix_server = ZabbixSender(zabbix_server_host)
     result = zabbix_server.send(send_data)
     result_info = {
         'failed': result.failed,
@@ -26,7 +36,6 @@ def send_metric_data(zabbix_data):
 
 def lambda_handler(event, context):
     zabbix_data = {
-        'zabbix_server_host': event['zabbix_server_host'],
         'target_host': event['target_host'],
         'target_key': event['target_key'],
         'target_value': event['target_value']
